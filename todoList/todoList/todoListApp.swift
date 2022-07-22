c6a99b2b0b973da6c2efce2b349655beff987416
umareddy <umareddy@umareddys-air.fritz.box>
2022-07-22 18:13:57 +0200

    first todolist commit

---
+++ todoList/todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by UMAREDDY on 20.07.22.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
